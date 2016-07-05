1d7b187b1d8b88814a7cf3197af1d91fa761f460
summer <626379451@qq.com>
2016-07-05 11:05:53 +0800

    整合

--- we/config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-                            return [
-                                'class' => 'yii\db\Connection',
-                                'dsn' => 'mysql:host=127.0.0.1;dbname=we7',
-                                'username' => 'root',
-                                'password' => 'root',
-                                'charset' => 'utf8',
-                            ];
